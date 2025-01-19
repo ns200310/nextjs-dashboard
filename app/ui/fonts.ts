76da41c51ef005447994cf982c944de26d6e550d
Nithika Senaweera <nsenaweera28@gmail.com>
2025-01-19 07:39:15 +0000

    feat: Added secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,6 @@
-import { Inter } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google";
+const inter = Inter({subsets: ['latin']})
+const lusitana = Lusitana({weight:["400", "700"], subsets: ["latin"]})
 
-export const inter = Inter({subsets: ['latin']})
+
+export {inter, lusitana}
