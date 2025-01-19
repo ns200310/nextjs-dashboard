4d9ccfa9f61985dce464d4bfd1d1160291d15ba7
Nithika Senaweera <nsenaweera28@gmail.com>
2025-01-19 22:20:19 +0000

    feat: populate the revenue chart in the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { lusitana } from "@/app/ui/fonts";
-
-export default function Page() {
+import { fetchRevenue } from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+export default async function Page() {
+    const revenue = await fetchRevenue();
     return (
         <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,7 +19,7 @@ export default function Page() {
           /> */}
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-          {/* <RevenueChart revenue={revenue}  /> */}
+          <RevenueChart revenue={revenue}  />
           {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
         </div>
       </main>
